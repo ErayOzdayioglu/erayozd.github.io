2d50d1d0d3ef236f73d5b6ed1c4d04dcd5c8ec08
ErayOzd <eray.ozdayioglu@kirapratik.com>
2021-10-13 11:57:14 +0300

    build

---
+++ build/precache-manifest.cdf7d828ede6754a0e892aa329d22903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f905af101141774a4b1b8ae4643ca5",
+    "url": "/react-terminal-portfolio/index.html"
+  },
+  {
+    "revision": "ffa6f8db7d2858e87466",
+    "url": "/react-terminal-portfolio/static/css/main.bf0095ea.chunk.css"
+  },
+  {
+    "revision": "9e76de35d71e578643ac",
+    "url": "/react-terminal-portfolio/static/js/2.4ee44813.chunk.js"
+  },
+  {
+    "revision": "ffa6f8db7d2858e87466",
+    "url": "/react-terminal-portfolio/static/js/main.a1ecf8be.chunk.js"
+  },
+  {
+    "revision": "e52e33c27f8acaf35c6b",
+    "url": "/react-terminal-portfolio/static/js/runtime-main.e3d21111.js"
+  }
+]);
