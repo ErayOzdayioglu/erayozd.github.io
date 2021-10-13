225eba64be9b0ffd5e9f416b832e81b69605a725
ErayOzd <eray.ozdayioglu@kirapratik.com>
2021-10-13 12:42:22 +0300

    Updates

---
+++ precache-manifest.fb506767258321135dd27da88b76dcb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d078d47e2a09e951b4a4b3f2e3b09b9a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "64326019c4c3c640c98c",
+    "url": "/portfolio/static/css/main.c901966c.chunk.css"
+  },
+  {
+    "revision": "f3021daeb73886111aa2",
+    "url": "/portfolio/static/js/2.f3df9ef7.chunk.js"
+  },
+  {
+    "revision": "64326019c4c3c640c98c",
+    "url": "/portfolio/static/js/main.0ec37503.chunk.js"
+  },
+  {
+    "revision": "6ef639eaa5f57c0f3bb2",
+    "url": "/portfolio/static/js/runtime-main.aba8ee4c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0546e2c20990183c666e0732397d80bb.js"
+  "/portfolio/precache-manifest.fb506767258321135dd27da88b76dcb3.js"
 );
 
 self.addEventListener('message', (event) => {
